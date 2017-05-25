c144a9fec57b315c15455cb18a3ddb944efb0957
Daniel Ruben de Brito <danieldebrito@outlook.com>
2017-05-25 13:35:00 -0300

    clase 14 metodo de extension, ejercicio echo en clase
    clase persona - clase persona dos hereda de una dll externa - clase
    persona sellada hereda desde una dll externa sellada, le incluimos
    metodos de extension

---
+++ clase 14 - metodos de extension/ejercicio en clase - personaSellada/persona/MetodExtenc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using personaLibreria;
+using Entidades.Externa.Sellada;
+
+
+namespace persona
+{
+    public static class MetodExtenc
+    {
+
+        public static string ObtenerInformacion(this PersonaExternaSellada ps)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Nombre: " + ps.Nombre);
+            sb.AppendLine("Apellido: " + ps.Apellido);
+            sb.AppendLine("Edad: " + ps.Edad);
+            sb.AppendLine("Sexo : " + ps.Sexo);
+
+            return sb.ToString();
+        }
+    }
+}
