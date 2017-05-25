c7f2e17e455f16192eed631713d8c9795f98d12b
Daniel Ruben de Brito <danieldebrito@outlook.com>
2017-05-25 12:51:18 -0300

    clase 14 metodo de estension
    /*
    * el metodo de extension se usa para agregarle metodos a clases selladas
    de las cuales no tenemos acceso
    * en este ejemplo se creo una clase sellada y luego se refencio a este
    proyecto desde el archivo dll que genera
    * mediante esta clase STATICA se crean los metodos de extension para ser
    impelentado en un proyecto de consola, clase Program
    */

---
+++ clase 14 - metodos de extension/met extension ejemplo sencillo/clase sellada/ClassLibrary1/Persona.Sellada.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreriaSellada
+{
+    public sealed class PersonaSellada
+    {
+        protected string _nombre;
+        protected int _edad;
+
+        public PersonaSellada(string nombre, int edad)
+        {
+            this._edad=edad;
+            this._nombre=nombre;
+        }
+
+        public string Nombre { get { return this._nombre; } }
+        public int edad { get { return this._edad; } }
+    }
+}
---
+++ clase 14 - metodos de extension/met extension ejemplo sencillo/metodoDeExtension/ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libreriaSellada;  // namespace de la clase sellada refenciada desde el archivo dll
+using persona;          // namespace de la libreria de clases que contiene la clase con los metodos de extension
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonaSellada p = new PersonaSellada("Raul de Brito", 78);
+
+            // implementacion del metodo de extension
+            Console.WriteLine(p.Mostrar());
+
+            Console.ReadKey();
+        }
+    }
+}
+
